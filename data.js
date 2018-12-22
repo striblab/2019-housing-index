242d2e99a1334270e9f7774b846175003692b134
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-12-22 16:11:55 -0500

    Stats.

--- data.js
+++ data.js
@@ -4,6 +4,7 @@
 
 // Dependencies
 const _ = require('lodash');
+const ss = require('simple-statistics');
 
 // Get data
 const areas = require('./sources/housing-index-areas.test.json');
@@ -11,7 +12,6 @@ const timeseries = require('./sources/housing-index-timeseries.test.json');
 
 // Process data
 function indexData() {
-  console.log(areas);
   /*
   FIELDS in the main table:
   GEOID = this is the Census place code (two-digit state FIPS, plus the place FIPS code)
@@ -36,19 +36,19 @@ function indexData() {
   */
 
   let parsed = _.map(areas, a => {
-    return {
+    let p = {
       geoid2: a.geoid2,
       name: a.Place,
       // a.FullName
       locationType: a.location,
       county: a.COUNTY,
       state: a.STATE,
-      index: a.Index_score,
-      rank: a.Index_rank,
-      rankPrevious: a.Last_rank,
-      closedSales2018: a.Cs_curr,
-      pricePerSqFtChange: a.Ppsf_pctchange,
-      daysOnMarketChange: a.Dom_diff,
+      index: a.index_score,
+      rank: a.index_rank,
+      rankPrevious: a.LastRank,
+      closedSales: a.cs_curr,
+      pricePerSqFtChange: a.ppsf_pctchange,
+      daysOnMarketChange: a.dom_diff,
       medianHomeValue: a.MedianValue,
       perPriceRecieved: a.pctorigprice,
       perNewConstruction: a.pctorigprice,
@@ -56,9 +56,100 @@ function indexData() {
       perCostBurdened: a.PctCostBurdenedOwners,
       medianHouseholdIncome: a.MedianHHIncome
     };
+
+    // Matched time series
+    /*
+    "Place": "Somerset",
+    "variable": "2003",
+    "closed": 116,
+    "geoid2": "5574675",
+    "FullName": "Somerset, WI",
+    "dom": 54.5,
+    "ppsf": 123.08,
+    "inventory": 50
+    */
+    let matched = _.filter(timeseries, { geoid2: p.geoid2 });
+    _.each(matched, m => {
+      let year = parseInt(m.variable, 10);
+
+      p.pricePerSqFtPerYear = p.pricePerSqFtPerYear || [];
+      if (m.ppsf || m.ppsf === 0) {
+        p.pricePerSqFtPerYear.push({ year, data: m.ppsf });
+      }
+
+      p.inventoryPerYear = p.inventoryPerYear || [];
+      if (m.inventory || m.inventory === 0) {
+        p.inventoryPerYear.push({ year, data: m.inventory });
+      }
+
+      p.closedPerYear = p.closedPerYear || [];
+      if (m.closed || m.closed === 0) {
+        p.closedPerYear.push({ year, data: m.closed });
+      }
+
+      p.daysOnMarketPerYear = p.daysOnMarketPerYear || [];
+      if (m.dom || m.dom === 0) {
+        p.daysOnMarketPerYear.push({ year, data: m.dom });
+      }
+    });
+
+    return p;
   });
 
-  return parsed;
+  // Make aggregate data
+  let stats = {};
+  _.each(
+    [
+      'closedSales',
+      'pricePerSqFtChange',
+      'daysOnMarketChange',
+      'medianHomeValue',
+      'medianHouseholdIncome'
+    ],
+    f => {
+      let s = _.filter(_.map(parsed, f), d => !_.isUndefined(d));
+
+      // Top level stats
+      stats[f] = stats[f] || {};
+      stats[f].count = s.length;
+      stats[f].min = ss.min(s);
+      stats[f].max = ss.max(s);
+      stats[f].median = ss.median(s);
+      stats[f].mean = ss.mean(s);
+      stats[f].std = ss.standardDeviation(s);
+
+      // Histogram numbers
+      let bins = Math.ceil(Math.sqrt(s.length));
+      let rawInterval = Math.abs((stats[f].max - stats[f].min) / bins);
+      let interval = parseFloat(rawInterval.toPrecision(1));
+      let intervalMin = parseFloat(stats[f].min.toPrecision(1));
+      // Manual fix
+      intervalMin = intervalMin < 2 ? 0 : intervalMin;
+
+      let histogram = [];
+      let max = intervalMin + interval;
+      let min = intervalMin;
+      while (max < stats[f].max) {
+        histogram.push({
+          min,
+          max,
+          count: _.filter(s, d => {
+            return d >= min && d < max;
+          }).length
+        });
+
+        min = max;
+        max = min + interval;
+      }
+
+      stats[f].histogram = histogram;
+    }
+  );
+
+  return {
+    areas: parsed,
+    stats
+  };
 }
 
 // Exports
